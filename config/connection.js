b8547d2c09f136d7fc5618a807b14b0a006bed12
Derek Sutton <dereksutton86@gmail.com>
2023-04-23 17:36:53 -0400

    updated database credentials and fixed connection.js file to remove visibility of credentials

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'master',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME || 'employee_tracker_db',
 });
 
